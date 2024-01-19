a8d841be5b77c0d2bf6731ab89df8a5f1d9d7c94
Jake Girhiny <jegirhiny@gmail.com>
2024-01-19 00:07:50 -0500

    clock added to navigation bar

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,13 +1,34 @@
 import './navigation.styles.css';
 import {useNavigate} from "react-router-dom";
+import {useState, useEffect} from 'react';
 
 const Navigation = () => {
     const navigate = useNavigate();
+    const [localTime, setLocalTime] = useState(getTime())
+
+    useEffect(() => {
+        const id = setInterval(() => {
+            setLocalTime(getTime());
+        }, 1000);
+
+        return () => clearInterval(id);
+    }, []);
+
+    function getTime() {
+        const options = {
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true,
+        };
+
+        return new Date().toLocaleTimeString('en-US', options);
+    }
 
     return (
         <div className='nav-bar'>
             <button onClick={() => navigate('/admin')}>Admin</button>
             <button onClick={() => navigate('/')}>Home</button>
+            <h3>{localTime}</h3>
         </div>
     )
 }
