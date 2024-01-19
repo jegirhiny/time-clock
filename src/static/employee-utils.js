36798fcc81d0f516a65e6bb7656b18dd8c3532ec
Jake Girhiny <jegirhiny@gmail.com>
2024-01-18 23:50:59 -0500

    enhanced employee utils with arrow operators

--- src/static/employee-utils.js
+++ src/static/employee-utils.js
@@ -9,42 +9,21 @@ const getEmployees = () => {
 }
 
 const getEmployeeByName = (value) => {
-    const employees = getEmployees();
-
-    return employees.filter((employee) => {
-        const fullName = employee.firstName + employee.lastName;
-
-        return fullName.includes(value.replace(' ', ''));
-    }, []);
+    return getEmployees().filter(employee => (employee.firstName + employee.lastName).includes(value.replace(' ', '')), []);
 }
 
 const createEmployee = ({firstName, lastName, password, isWorking}) => {
-    if(hasEmployees()) {
-        const prevEmployees = JSON.parse(localStorage.getItem('employees'));
-
-        localStorage.setItem('employees', JSON.stringify([...prevEmployees, {id: uuidv4(), firstName, lastName, password, isWorking}]));
-    } else {
-        localStorage.setItem('employees', JSON.stringify([{id: uuidv4(), firstName, lastName, password, isWorking}]));
-    }
-}
-
-const getEmployeeIndex = (id) => {
-    return getEmployees().findIndex((employee) => employee.id === id);
+    const prevEmployees = hasEmployees() ? JSON.parse(localStorage.getItem('employees')) : [];
+    
+    localStorage.setItem('employees', JSON.stringify([...prevEmployees, {id: uuidv4(), firstName, lastName, password, isWorking}]));
 }
 
 const removeEmployee = (id) => {
-    localStorage.setItem('employees', JSON.stringify(getEmployees().filter((employee) => employee.id !== id)));
+    localStorage.setItem('employees', JSON.stringify(getEmployees().filter(employee => employee.id !== id)));
 }
 
 const setIsWorking = (id) => {
-    const employee = (JSON.parse(localStorage.getItem('employees'))).find((employee) => employee.id === id);
-    employee.isWorking = !employee.isWorking;
-
-    console.log(employee);
+    localStorage.setItem('employees', JSON.stringify(getEmployees().map(employee => (employee.id === id) ? { ...employee, isWorking: !employee.isWorking } : employee)));
 }
 
-const getIsWorking = (id) => {
-    // const employee = (JSON.parse(localStorage.getItem('employees'))).filter((employee) => employee.id === employee);
-}
-
-export { hasEmployees, getEmployees, getEmployeeByName, createEmployee, setIsWorking, getIsWorking, removeEmployee};
+export { hasEmployees, getEmployees, getEmployeeByName, createEmployee, setIsWorking, removeEmployee};
