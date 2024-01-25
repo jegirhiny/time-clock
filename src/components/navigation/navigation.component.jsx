bcc172fb75bf289a188e2a11d331940e5bbc892f
Jake Girhiny <jegirhiny@gmail.com>
2024-01-25 00:16:50 -0500

    footer addition to navigation bar

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -19,21 +19,26 @@ const Navigation = () => {
     }, []);
 
     return (
-        <div className='marg-right nav-bar'>
-            <div className='header'>
-                <label>Clockify Navigation</label>
+        <div>
+            <div className='marg-right nav-bar'>
+                <div className='header'>
+                    <label>Clockify Navigation</label>
+                    <div className='nav-container'>
+                        <img className='image' src={Clock} alt="Clock Icon" />
+                        <h3 className='nav-text'>{localTime}</h3>
+                    </div>
+                    <div className='nav-container cursor' onClick={() => navigate('/admin')}>
+                        <img className='image' src={Cog} alt="Cog Icon" />
+                        <h3 className='nav-text'>Admin</h3>
+                    </div>
+                    <div className='nav-container cursor' onClick={() => navigate('/')}>
+                        <img className='image' src={Home} alt="Home Icon" />
+                        <h3 className='nav-text'>Home</h3>
+                    </div>
+                </div>
             </div>
-            <div className='nav-container'>
-                <img className='image' src={Clock} alt="Clock Icon" />
-                <h3 className='nav-text'>{localTime}</h3>
-            </div>
-            <div className='nav-container cursor' onClick={() => navigate('/admin')}>
-                <img className='image' src={Cog} alt="Cog Icon" />
-                <h3 className='nav-text'>Admin</h3>
-            </div>
-            <div className='nav-container cursor' onClick={() => navigate('/')}>
-                <img className='image' src={Home} alt="Home Icon" />
-                <h3 className='nav-text'>Home</h3>
+            <div className='footer dark'>
+                <h3>Clockify — Time Clock</h3>
             </div>
         </div>
     )
